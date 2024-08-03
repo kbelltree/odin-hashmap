3b4482754cb19c940184de1354015c6795ffe243
KBelltree <98202109+kbelltree@users.noreply.github.com>
2024-08-03 00:17:06 -0700

    Apply linters

--- hashMap.js
+++ hashMap.js
@@ -1,272 +1,272 @@
 class HashMap {
-    constructor(capacity = 16, loadFactor = 0.75){
-        // track initial capacity for clear method
-        this.initialCapacity = capacity; 
-        this.capacity = capacity; 
-        this.loadFactor = loadFactor; 
-        this.buckets = Array.from({ length: this.capacity }, () => []);
-        this.dataCount = 0;
+  constructor(capacity = 16, loadFactor = 0.75) {
+    // track initial capacity for clear method
+    this.initialCapacity = capacity;
+    this.capacity = capacity;
+    this.loadFactor = loadFactor;
+    this.buckets = Array.from({ length: this.capacity }, () => []);
+    this.dataCount = 0;
+  }
+
+  hash(key) {
+    if (typeof key !== "string" || key === "") {
+      throw new Error("Only non-empty strings are accepted as key");
+    }
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
+
+  set(key, value) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+    const targetBucket = this.buckets[index];
+
+    // overwrite with the new value if the key already exists
+    for (let data of targetBucket) {
+      if (data[0] === key) {
+        data[1] = value;
+        return;
+      }
     }
 
-    hash(key) {
-        if (typeof key !== "string" || key === ""){
-            throw new Error("Only non-empty strings are accepted as key");
-        }
-        let hashCode = 0;       
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
-        }    
-        return hashCode;
-      } 
-    
-    set(key, value) {
-        const index = this.hash(key); 
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }  
-        const targetBucket = this.buckets[index];
+    // otherwise add the new key value data
+    targetBucket.push([key, value]);
 
-        // overwrite with the new value if the key already exists
-        for (let data of targetBucket) {
-            if(data[0] === key){
-                data[1]= value;
-                return; 
-            } 
-        }
-        
-        // otherwise add the new key value data
-        targetBucket.push([key, value]);
-         
-        // expand the capacity if the buckets are nearly full
-        const threshold = this.capacity * this.loadFactor; 
-        if (threshold <= this.dataCount){
-            this.buckets = this.expandCapacity();
-        }       
-        this.dataCount++;  
+    // expand the capacity if the buckets are nearly full
+    const threshold = this.capacity * this.loadFactor;
+    if (threshold <= this.dataCount) {
+      this.buckets = this.expandCapacity();
     }
+    this.dataCount++;
+  }
 
-    get(key){
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let data of targetBucket) {
-            if(data[0] === key) {
-                return data[1]; 
-            }   
-        }
-        return null;  
+  get(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let data of targetBucket) {
+      if (data[0] === key) {
+        return data[1];
+      }
+    }
+    return null;
+  }
 
-    has(key) {
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let data of targetBucket) {
-            if(data[0] === key) {
-                return true; 
-            }   
-        }
-        return false; 
+  has(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let data of targetBucket) {
+      if (data[0] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-    remove(key){
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let i = 0; i < targetBucket.length; i++){
-            if(targetBucket[i][0] === key){
-                targetBucket.splice(i, 1);
-                this.dataCount--;
-                return true; 
-            }
-        }
-        return false; 
+  remove(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i][0] === key) {
+        targetBucket.splice(i, 1);
+        this.dataCount--;
+        return true;
+      }
+    }
+    return false;
+  }
 
-    length() {
-        let counter = 0; 
-        for (let i = 0; i < this.buckets.length; i++){
-            counter += this.buckets[i].length; 
-        }
-        return counter;
+  length() {
+    let counter = 0;
+    for (let i = 0; i < this.buckets.length; i++) {
+      counter += this.buckets[i].length;
     }
+    return counter;
+  }
 
-    clear() {
-        this.capacity = this.initialCapacity; 
-        this.buckets = Array.from({ length: this.initialCapacity }, () => []);
-        this.dataCount = 0;
-    }
+  clear() {
+    this.capacity = this.initialCapacity;
+    this.buckets = Array.from({ length: this.initialCapacity }, () => []);
+    this.dataCount = 0;
+  }
 
-    keys() {
-        let array = [];
-        this.buckets.forEach((bucket) => {
-            bucket.forEach(data => {
-                array.push(data[0]);
-            });
-        })
-        return array; 
-    }
+  keys() {
+    let array = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((data) => {
+        array.push(data[0]);
+      });
+    });
+    return array;
+  }
 
-    values(){
-        let array = [];
-        this.buckets.forEach((bucket) => {
-            bucket.forEach(data => {
-                array.push(data[1]);
-            });
-        })
-        return array; 
-    }
+  values() {
+    let array = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((data) => {
+        array.push(data[1]);
+      });
+    });
+    return array;
+  }
 
-    entries() {
-        let array = [];
-        this.buckets.forEach((bucket) => {
-            bucket.forEach((data) => {
-                array.push(data);
-            });
-        })
-        return array; 
-    }
+  entries() {
+    let array = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((data) => {
+        array.push(data);
+      });
+    });
+    return array;
+  }
 
-    // grow capacity to hold total data count
-    expandCapacity() {
-        this.capacity *= 2; 
-        let newBuckets = Array.from({ length: this.capacity }, () => []);
-        const deHashedData = this.entries();
-        
-        // rehash each data and store in the new buckets
-        deHashedData.forEach((data) => {
-            let index = this.hash(data[0]);      
-            newBuckets[index].push(data); 
-        })
-        return newBuckets; 
-    }
+  // grow capacity to hold total data count
+  expandCapacity() {
+    this.capacity *= 2;
+    let newBuckets = Array.from({ length: this.capacity }, () => []);
+    const deHashedData = this.entries();
+
+    // rehash each data and store in the new buckets
+    deHashedData.forEach((data) => {
+      let index = this.hash(data[0]);
+      newBuckets[index].push(data);
+    });
+    return newBuckets;
+  }
 }
 
 class HashSet {
-    constructor(capacity = 16, loadFactor = 0.75){
-        this.initialCapacity = capacity; 
-        this.capacity = capacity; 
-        this.loadFactor = loadFactor; 
-        this.buckets = Array.from({ length: this.capacity }, () => []);
-        this.dataCount = 0;
-    }
+  constructor(capacity = 16, loadFactor = 0.75) {
+    this.initialCapacity = capacity;
+    this.capacity = capacity;
+    this.loadFactor = loadFactor;
+    this.buckets = Array.from({ length: this.capacity }, () => []);
+    this.dataCount = 0;
+  }
 
-    hash(key) {
-        if (typeof key !== "string" || key === ""){
-            throw new Error("Only non-empty strings are accepted as key");
-        }
-        let hashCode = 0;       
-        const primeNumber = 31;
-        for (let i = 0; i < key.length; i++) {
-          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
-        }    
-        return hashCode;
-      } 
-    
-    set(key) {
-        const index = this.hash(key); 
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }  
-        const targetBucket = this.buckets[index];
-        for (let i = 0; i < targetBucket.length; i++){
-            if (targetBucket[i] === key){
-                return; 
-            } 
-        }
-        targetBucket.push(key);        
-        const threshold = this.capacity * this.loadFactor; 
-        if (threshold <= this.dataCount){
-            this.buckets = this.expandCapacity();
-        }       
-        this.dataCount++;  
+  hash(key) {
+    if (typeof key !== "string" || key === "") {
+      throw new Error("Only non-empty strings are accepted as key");
     }
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+    return hashCode;
+  }
 
-    get(key){
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let i = 0; i < targetBucket.length; i++) {
-            if(targetBucket[i] === key) {    
-                return key; 
-            }   
-        }
-        return null;  
+  set(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i] === key) {
+        return;
+      }
+    }
+    targetBucket.push(key);
+    const threshold = this.capacity * this.loadFactor;
+    if (threshold <= this.dataCount) {
+      this.buckets = this.expandCapacity();
+    }
+    this.dataCount++;
+  }
 
-    has(key) {
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let i = 0; i < targetBucket.length; i++) {
-            if(targetBucket[i] === key) {
-                return true; 
-            }   
-        }
-        return false; 
+  get(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i] === key) {
+        return key;
+      }
+    }
+    return null;
+  }
 
-    remove(key){
-        const index = this.hash(key);
-        if (index < 0 || index >= this.buckets.length) {
-            throw new Error("Trying to access index out of bound");
-        }
-        const targetBucket = this.buckets[index];
-        for (let i = 0; i < targetBucket.length; i++){
-            if(targetBucket[i] === key){
-                targetBucket.splice(i, 1);
-                this.dataCount--;
-                return true; 
-            }
-        }
-        return false; 
+  has(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-    length() {
-        let counter = 0; 
-        for (let i = 0; i < this.buckets.length; i++){
-            counter += this.buckets[i].length; 
-        }
-        return counter;
+  remove(key) {
+    const index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bound");
     }
+    const targetBucket = this.buckets[index];
+    for (let i = 0; i < targetBucket.length; i++) {
+      if (targetBucket[i] === key) {
+        targetBucket.splice(i, 1);
+        this.dataCount--;
+        return true;
+      }
+    }
+    return false;
+  }
 
-    clear() {
-        this.capacity = this.initialCapacity; 
-        this.buckets = Array.from({ length: this.initialCapacity }, () => []);
-        this.dataCount = 0;
+  length() {
+    let counter = 0;
+    for (let i = 0; i < this.buckets.length; i++) {
+      counter += this.buckets[i].length;
     }
+    return counter;
+  }
 
-    keys() {
-        let array = [];
-        this.buckets.forEach((bucket) => {
-            bucket.forEach(key => {
-                array.push(key);
-            });
-        })
-        return array; 
-    }
+  clear() {
+    this.capacity = this.initialCapacity;
+    this.buckets = Array.from({ length: this.initialCapacity }, () => []);
+    this.dataCount = 0;
+  }
 
-    expandCapacity() {
-        this.capacity *= 2; 
-        let newBuckets = Array.from({ length: this.capacity }, () => []);
-        const deHashedData = this.keys();     
-        deHashedData.forEach((key) => {
-            let index = this.hash(key);     
-            newBuckets[index].push(key); 
-        })
-        return newBuckets; 
-    }
+  keys() {
+    let array = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((key) => {
+        array.push(key);
+      });
+    });
+    return array;
+  }
+
+  expandCapacity() {
+    this.capacity *= 2;
+    let newBuckets = Array.from({ length: this.capacity }, () => []);
+    const deHashedData = this.keys();
+    deHashedData.forEach((key) => {
+      let index = this.hash(key);
+      newBuckets[index].push(key);
+    });
+    return newBuckets;
+  }
 }
 
 module.exports = { HashMap, HashSet };
--- test.js
+++ test.js
@@ -1,135 +1,134 @@
-const { HashMap, HashSet } = require('./hashMap');
+const { HashMap, HashSet } = require("./hashMap");
 
 // Create a new instance of your hash map and set the load factor to be 0.75.
 const test = new HashMap();
 
 // Populate your hash map using the set(key, value) method by copying the given combos:
-test.set('apple', 'red');
-test.set('banana', 'yellow');
-test.set('carrot', 'orange');
-test.set('dog', 'brown');
-test.set('elephant', 'gray');
-test.set('frog', 'green');
-test.set('grape', 'purple');
-test.set('hat', 'black');
-test.set('ice cream', 'white');
-test.set('jacket', 'blue');
-test.set('kite', 'pink');
-test.set('lion', 'golden');
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("lion", "golden");
 
 console.dir(test, { depth: null });
 
 //  Try overwriting a few nodes using set(key, value). This should only over-write the existing values of your nodes and not add new ones.
-test.set('kite', 'yellow');
-test.set('lion', 'brown');
+test.set("kite", "yellow");
+test.set("lion", "brown");
 
 console.dir(test, { depth: null });
 
-// Populate your hash map with the last node (doing this will make your hash map exceed your current load factor, hence expanding your buckets and growing your hash map). 
-test.set('moon', 'silver');
+// Populate your hash map with the last node (doing this will make your hash map exceed your current load factor, hence expanding your buckets and growing your hash map).
+test.set("moon", "silver");
 
 console.dir(test, { depth: null });
-console.log('length():', test.length());
+console.log("length():", test.length());
 
 // With your new hash map, try overwriting a few nodes using set(key, value). This should only over-write existing values of your nodes.
 
-test.set('moon', 'gray');
-test.set('jacket', 'navy');
-test.set('to-remove', 'no-color'); 
+test.set("moon", "gray");
+test.set("jacket", "navy");
+test.set("to-remove", "no-color");
 
 console.dir(test, { depth: null });
-console.log('length():', test.length());
+console.log("length():", test.length());
 
 // Test the other methods of your hash maps such as get(key), has(key), remove(key), length(), clear(), keys(), values(), and entries().
-console.log('get():', test.get('moon'));
-console.log('get():', test.get('tomato'));
-console.log('get():', test.get('ice cream'));
-console.log('has():', test.has('moon'));
-console.log('has():', test.has('tomato'));
-console.log('has():', test.has('ice cream'));
-console.log('remove():', test.remove('to-remove'));
-console.log('remove():', test.remove('tomato'));
-console.log('has():', test.has('to-remove')); 
+console.log("get():", test.get("moon"));
+console.log("get():", test.get("tomato"));
+console.log("get():", test.get("ice cream"));
+console.log("has():", test.has("moon"));
+console.log("has():", test.has("tomato"));
+console.log("has():", test.has("ice cream"));
+console.log("remove():", test.remove("to-remove"));
+console.log("remove():", test.remove("tomato"));
+console.log("has():", test.has("to-remove"));
 
 console.dir(test, { depth: null });
 
-test.set('skirt', 'black');
-test.set('lime', 'green'); 
+test.set("skirt", "black");
+test.set("lime", "green");
 
 // test.set('', '');
 // test.set(123, 'number');
 
 console.dir(test, { depth: null });
-console.log('length():', test.length());
+console.log("length():", test.length());
 
-console.log('keys():', test.keys());
-console.log('values():', test.values());
-console.log('entries():', test.entries());
+console.log("keys():", test.keys());
+console.log("values():", test.values());
+console.log("entries():", test.entries());
 
-test.clear(); 
-console.log('clear():', test);
-console.log('keys():', test.keys());
-console.log('entries():', test.entries());
-
+test.clear();
+console.log("clear():", test);
+console.log("keys():", test.keys());
+console.log("entries():", test.entries());
 
 // HashSet tests
 
 const test2 = new HashSet();
 
-test2.set('apple');
-test2.set('banana');
-test2.set('carrot');
-test2.set('dog');
-test2.set('elephant');
-test2.set('frog');
-test2.set('grape');
-test2.set('hat');
-test2.set('ice cream');
-test2.set('jacket');
-test2.set('kite');
-test2.set('lion');
+test2.set("apple");
+test2.set("banana");
+test2.set("carrot");
+test2.set("dog");
+test2.set("elephant");
+test2.set("frog");
+test2.set("grape");
+test2.set("hat");
+test2.set("ice cream");
+test2.set("jacket");
+test2.set("kite");
+test2.set("lion");
 
 console.dir(test2, { depth: null });
 
-test2.set('kite');
-test2.set('lion');
+test2.set("kite");
+test2.set("lion");
 
 console.dir(test2, { depth: null });
 
-test2.set('moon');
+test2.set("moon");
 
 console.dir(test2, { depth: null });
-console.log('length():', test2.length());
+console.log("length():", test2.length());
 
-test2.set('moon');
-test2.set('jacket');
-test2.set('to-remove'); 
-
-console.dir(test2, { depth: null });
-console.log('length():', test2.length());
-
-console.log('get():', test2.get('moon'));
-console.log('get():', test2.get('tomato'));
-console.log('get():', test2.get('ice cream'));
-console.log('has():', test2.has('moon'));
-console.log('has():', test2.has('tomato'));
-console.log('has():', test2.has('ice cream'));
-console.log('remove():', test2.remove('to-remove'));
-console.log('remove():', test2.remove('tomato'));
-console.log('has():', test2.has('to-remove')); 
+test2.set("moon");
+test2.set("jacket");
+test2.set("to-remove");
 
 console.dir(test2, { depth: null });
+console.log("length():", test2.length());
 
-test2.set('skirt');
-test2.set('lime'); 
+console.log("get():", test2.get("moon"));
+console.log("get():", test2.get("tomato"));
+console.log("get():", test2.get("ice cream"));
+console.log("has():", test2.has("moon"));
+console.log("has():", test2.has("tomato"));
+console.log("has():", test2.has("ice cream"));
+console.log("remove():", test2.remove("to-remove"));
+console.log("remove():", test2.remove("tomato"));
+console.log("has():", test2.has("to-remove"));
+
+console.dir(test2, { depth: null });
+
+test2.set("skirt");
+test2.set("lime");
 
 // test2.set('');
 // test2.set(123, 'number');
 
 console.dir(test2, { depth: null });
 
-console.log('keys():', test2.keys());
+console.log("keys():", test2.keys());
 
-test2.clear(); 
-console.log('clear():', test2);
-console.log('keys():', test2.keys());
+test2.clear();
+console.log("clear():", test2);
+console.log("keys():", test2.keys());
